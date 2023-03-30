e6727215e1760770892881c09fb5f6549f9b0c77
YulianaNggohele <125204172+YulianaNggohele@users.noreply.github.com>
2023-03-29 20:15:04 -0700

    Add files via upload

---
+++ TugasMID/TestErr0175.java
@@ -0,0 +1,16 @@
+//Nama : Yuliana Nggohele
+//Nim  : 13020210175
+
+import java.util.Scanner;
+
+public class TestErr0175 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int i = input.nextInt();
+        int k = 100;
+        char c = 's';
+        boolean falses = false;
+        int j = i + 1;
+        System.out.println("j adalah " + j + " k adalah " + k + " dan c adalah " + c + " dan falses adalah " + falses);
+    }
+}
